5a81689d0869f6be565da7641991dd371fb4636c
Guy Langston <guylangston@gmail.com>
2021-11-11 16:11:28 +0000

    Added python and versions

--- primes-upto-niave-1c-nomem/run.sh
+++ primes-upto-niave-1c-nomem/run.sh
@@ -19,14 +19,21 @@ for p in $proj; do
 done
 
 echo "[MONO]"
+mono --version
 mcs -optimize+ Program.cs -out:./bin/Program-mono.exe
 mono ./bin/Program-mono.exe
 
 echo ""
 echo "[gcc]"
+gcc --version
 gcc bench.c -O3 -lm --output ./bin/bench-gcc && ./bin/bench-gcc
 
 echo ""
 echo "[clang]"
+clang --version
 clang bench.c -O3 -lm --output ./bin/bench-clang && ./bin/bench-clang
 
+echo ""
+echo "[python] Slow and I am not sure why?"
+python --version
+time python ./bench.py
