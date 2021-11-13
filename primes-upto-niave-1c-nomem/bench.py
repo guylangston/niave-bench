ad498f8f49b37b4c3b445da953a5139b9cb7ab5c
Guy Langston <guylangston@gmail.com>
2021-11-13 09:09:59 +0000

    python

--- primes-upto-niave-1c-nomem/bench.py
+++ primes-upto-niave-1c-nomem/bench.py
@@ -12,9 +12,9 @@ def find_primes(upto):
     print("Found:", found, ", LastPrime:", lastPrime)
 
 def is_prime(num):
-    max = int(math.sqrt(num))
+    max = int(num // 2)
     for ii in range(3, max, 2):
-        if (num % ii == 0): return True
+        if (num % ii) == 0: return True
     return False
 
 find_primes(16000000)
